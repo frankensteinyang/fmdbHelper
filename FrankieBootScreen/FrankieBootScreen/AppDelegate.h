48a60cd45977eb6578d49e8fba2f5735fd9660cf
杨进忠 <fred.is.almighty@usa.com>
2015-02-11 17:37:37 +0800

    yangjinzhong

---
+++ FrankieBootScreen/FrankieBootScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/11/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrankieBootScreen/FrankieBootScreen/FrankieSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrankieSplashViewController.h
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/11/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrankieSplashViewController : UIViewController
+
+@end
