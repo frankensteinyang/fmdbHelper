cb2a03b4388b915225ba3ca1827c4b0e0e108728
杨进忠 <fred.is.almighty@usa.com>
2015-02-14 23:49:42 +0800

    RootNavigationController created.

---
+++ FrankieBootScreen/FrankieBootScreen/FrankieRootNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FrankieRootNavigationController.h
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/14/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrankieRootNavigationController : UINavigationController
+
+@end
