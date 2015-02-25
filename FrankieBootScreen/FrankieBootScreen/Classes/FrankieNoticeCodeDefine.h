4312adb75a2f8762d488f16bd71c77a1187ad003
杨进忠 <fred.is.almighty@usa.com>
2015-02-25 23:51:07 +0800

    Wed Feb 25th 2015

---
+++ FrankieBootScreen/FrankieBootScreen/Classes/FrankieNoticeCodeDefine.h
@@ -0,0 +1,20 @@
+//
+//  FrankieNoticeCodeDefine.h
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/25/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#ifndef FrankieBootScreen_FrankieNoticeCodeDefine_h
+#define FrankieBootScreen_FrankieNoticeCodeDefine_h
+
+typedef enum CodeType {
+
+    NoticeCodeLaunched = 1001,
+
+} NoticeCodeType;
+
+
+
+#endif
