b5b8de4c3ec8a0ae8a5403b28064d7d6cf167f51
杨进忠 <fred.is.almighty@usa.com>
2015-02-26 15:43:17 +0800

    Thu Feb 26 3:43 PM 2015

---
+++ FrankieBootScreen/FrankieBootScreen/Classes/FrankieProcessManager.h
@@ -0,0 +1,13 @@
+//
+//  FrankieProcessManager.h
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/26/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrankieProcessManager : NSObject
+
+@end
---
+++ FrankieBootScreen/FrankieBootScreen/Classes/FrankieProcessManager.m
@@ -0,0 +1,13 @@
+//
+//  FrankieProcessManager.m
+//  FrankieBootScreen
+//
+//  Created by Frankenstein Yang on 2/26/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FrankieProcessManager.h"
+
+@implementation FrankieProcessManager
+
+@end
