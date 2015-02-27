6a69405df4c299a6d57ff4f939c75a67c7b4b9b1
杨进忠 <fred.is.almighty@usa.com>
2015-02-27 09:50:06 +0800

    Fri Feb 27th 9:50 AM 2015

---
+++ FrankieAmap/FrankieAmap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 2/27/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrankieAmap/FrankieAmap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 2/27/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
