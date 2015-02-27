2381bfbf4bbf48020d7dade497197140b5e66a7b
杨进忠 <fred.is.almighty@usa.com>
2015-02-27 16:00:10 +0800

    Fri Feb 27th 4:00 PM 2015

--- FrankieAmap/FrankieAmap/ViewController.m
+++ FrankieAmap/FrankieAmap/ViewController.m
@@ -7,12 +7,35 @@
 //
 
 #import "ViewController.h"
+#import <MAMapKit/MAMapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MAMapViewDelegate> {
+
+    MAMapView *_mapView;
+    
+}
 
 @end
 
 @implementation ViewController
+
+/**
+ *  .构造MAMapView对象；
+ 
+ 2.设置代理；
+ 
+ 3.将MAMapView添加到Subview中。
+ */
+
+- (void)viewDidAppear:(BOOL)animated {
+
+    [super viewDidAppear:animated];
+    [MAMapServices sharedServices].apiKey = @"8eb3e00b5959c32ca542f97a6de80e5f";
+    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
+    _mapView.delegate = self;
+    [self.view addSubview:_mapView];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
