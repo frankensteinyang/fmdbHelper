2d7a9c6207cdc5df8b732c3a9866e55f4e210896
杨进忠 <fred.is.almighty@usa.com>
2015-03-02 23:08:10 +0800

    Mon Mar 2 11:08 PM 2015

---
+++ FrankieAmap/FrankieAmap/Classes/FrankieBaseMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrankieBaseMapViewController.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/2/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrankieBaseMapViewController : UIViewController
+
+@end
