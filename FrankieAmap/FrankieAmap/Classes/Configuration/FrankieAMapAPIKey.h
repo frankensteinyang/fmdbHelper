8197ce114f864a481e445c4d15ac45d37ace7450
杨进忠 <fred.is.almighty@usa.com>
2015-03-03 23:16:14 +0800

    Tue Mar 3rd 11:16 PM 2015

---
+++ FrankieAmap/FrankieAmap/Classes/Configuration/FrankieAMapAPIKey.h
@@ -0,0 +1,13 @@
+//
+//  FrankieAMapAPIKey.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/3/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#ifndef FrankieAmap_FrankieAMapAPIKey_h
+#define FrankieAmap_FrankieAMapAPIKey_h
+
+
+#endif
