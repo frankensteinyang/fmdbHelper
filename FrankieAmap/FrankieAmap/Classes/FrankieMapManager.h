9e68cc04736023b3c7d42deef11f4728e0f4bb31
杨进忠 <fred.is.almighty@usa.com>
2015-03-05 23:43:08 +0800

    Thu Mar 5th 11:43 PM 2015

---
+++ FrankieAmap/FrankieAmap/Classes/FrankieMapManager.h
@@ -0,0 +1,13 @@
+//
+//  FrankieMapManager.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/5/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrankieMapManager : NSObject
+
+@end
---
+++ FrankieAmap/FrankieAmap/Classes/FrankieMapManager.m
@@ -0,0 +1,13 @@
+//
+//  FrankieMapManager.m
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/5/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FrankieMapManager.h"
+
+@implementation FrankieMapManager
+
+@end
