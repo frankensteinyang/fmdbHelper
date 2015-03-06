98f1679f133728a3ae72403b5056809fc7207d60
杨进忠 <fred.is.almighty@usa.com>
2015-03-06 09:36:18 +0800

    Created MainViewController

---
+++ FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrankieMainViewController.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/6/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FrankieMainViewController : ViewController
+
+@end
---
+++ FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.m
@@ -0,0 +1,13 @@
+//
+//  FrankieMainViewController.m
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/6/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FrankieMainViewController.h"
+
+@implementation FrankieMainViewController
+
+@end
