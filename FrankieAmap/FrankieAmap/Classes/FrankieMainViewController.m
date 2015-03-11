40bdf765331919b08ca07db3e99abadd7d4d225e
杨进忠 <fred.is.almighty@usa.com>
2015-03-11 23:47:13 +0800

    MAMapView

--- FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.m
+++ FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.m
@@ -9,62 +9,21 @@
 #import "FrankieMainViewController.h"
 #import <MAMapKit/MAMapKit.h>
 
-@interface FrankieMainViewController () <MAMapViewDelegate> {
+@interface FrankieMainViewController () <MAMapViewDelegate>
 
-    MAMapView *_mapView;
-
-}
-
-@property (nonatomic, retain)UISegmentedControl *showSegment;
-
-@property (nonatomic, retain)UISegmentedControl *modeSegment;
+@property (weak, nonatomic) IBOutlet MAMapView *mapView;
+@property (strong, nonatomic) MAPointAnnotation *centerAnno;
+@property (strong, nonatomic) MAPinAnnotationView *pinAnnoView;
 
 @end
 
 @implementation FrankieMainViewController
 
-@synthesize showSegment, modeSegment;
-
-- (void)initToolBar
-{
-    UIBarButtonItem *flexble = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
-                                                                             target:nil
-                                                                             action:nil];
-    
-    self.showSegment = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Start", @"Stop", nil]];
-    [self.showSegment addTarget:self action:@selector(showsSegmentAction:) forControlEvents:UIControlEventValueChanged];
-    self.showSegment.selectedSegmentIndex = 0;
-    UIBarButtonItem *showItem = [[UIBarButtonItem alloc] initWithCustomView:self.showSegment];
-    
-    self.modeSegment = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"None", @"Follow", @"Head", nil]];
-    [self.modeSegment addTarget:self action:@selector(modeAction:) forControlEvents:UIControlEventValueChanged];
-    self.modeSegment.selectedSegmentIndex = 0;
-    UIBarButtonItem *modeItem = [[UIBarButtonItem alloc] initWithCustomView:self.modeSegment];
-    
-    self.toolbarItems = [NSArray arrayWithObjects:flexble, showItem, flexble, modeItem, flexble, nil];
-}
-
-#pragma mark - Action Handle
-
-- (void)showsSegmentAction:(UISegmentedControl *)sender
-{
-    _mapView.showsUserLocation = !sender.selectedSegmentIndex;
-}
-
-- (void)modeAction:(UISegmentedControl *)sender
-{
-    _mapView.userTrackingMode = sender.selectedSegmentIndex;
-}
-
 - (void)viewDidAppear:(BOOL)animated {
     
     [super viewDidAppear:animated];
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
-    _mapView.delegate = self;
-    _mapView.showsUserLocation = YES;
-    _mapView.showsCompass = NO;
-    _mapView.showsScale = NO;
-    [self.view addSubview:_mapView];
+    self.mapView.showsCompass = NO;
+    self.mapView.showsScale = NO;
     
 }
 
@@ -75,19 +34,11 @@
     _mapView.userTrackingMode = MAUserTrackingModeFollow;
     _mapView.centerCoordinate = CLLocationCoordinate2DMake(31.220032, 121.369915);
     
-    [self initToolBar];
-    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (MAOverlayRenderer *)mapView:(MAMapView *)mapView rendererForOverlay:(id<MAOverlay>)overlay {
-    
-    return nil;
-    
 }
 
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
