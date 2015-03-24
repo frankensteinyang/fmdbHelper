0e550a0f7fc6a9b35318aefcc05b3205e9dd92ce
杨进忠 <fred.is.almighty@usa.com>
2015-03-24 12:30:13 +0800

    Google Maps Created congrollers

---
+++ FrankieAmap/FrankieAmap/Classes/Google Maps/Controllers/FrankieGoogleMapsMyLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrankieGoogleMapsMyLocationViewController.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/24/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrankieGoogleMapsMyLocationViewController : UIViewController
+
+@end
---
+++ FrankieAmap/FrankieAmap/Classes/Google Maps/Controllers/FrankieGoogleMapsMyLocationViewController.m
@@ -0,0 +1,37 @@
+//
+//  FrankieGoogleMapsMyLocationViewController.m
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 3/24/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FrankieGoogleMapsMyLocationViewController.h"
+
+@interface FrankieGoogleMapsMyLocationViewController ()
+
+@end
+
+@implementation FrankieGoogleMapsMyLocationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
