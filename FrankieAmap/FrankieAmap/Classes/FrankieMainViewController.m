26b4632b7e8f69ac43ed947cb07e6184810e50b2
杨进忠 <fred.is.almighty@usa.com>
2015-04-04 23:56:49 +0800

    Sat Apr 4th 2015 11:56 PM

--- FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.m
+++ FrankieAmap/FrankieAmap/Classes/FrankieMainViewController.m
@@ -47,7 +47,7 @@
     _mapView.showsScale = YES;
     _mapView.centerCoordinate = CLLocationCoordinate2DMake(31.220032, 121.369915);
     
-    
+    // 不显示定位点
     CLLocationCoordinate2D userLocation = CLLocationCoordinate2DMake(31.220032, 121.369915);
     MACoordinateSpan span = MACoordinateSpanMake(MAP_COORDINATE_SPAN, MAP_COORDINATE_SPAN);
     MACoordinateRegion region = {userLocation, span};
@@ -61,6 +61,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+/**
+ *  不进代理方法
+ *
+ *  @param mapView          <#mapView description#>
+ *  @param userLocation     <#userLocation description#>
+ *  @param updatingLocation <#updatingLocation description#>
+ */
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
     
     NSLog(@"BUG:代理方法没有实现。FrankieMapHelper");
@@ -77,6 +84,15 @@
     userLocationView = [[MAAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"userLocationView"];
     userLocationView.image = [UIImage imageNamed:@"AccuracyCircle.png"];
     return userLocationView;
+    
+}
+
+- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation {
+
+    NSLog(@"BUG:代理方法没有实现。FrankieMapHelper");
+    if (userLocation) {
+        NSLog(@"Latitude : %f, Longitude: %f", userLocation.coordinate.latitude, userLocation.coordinate.longitude);
+    }
     
 }
 
