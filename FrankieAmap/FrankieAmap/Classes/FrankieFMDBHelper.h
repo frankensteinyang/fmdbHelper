726a5793a3f0280f31fb9d6eea1c310dc71ccae0
杨进忠 <fred.is.almighty@usa.com>
2015-05-14 23:16:10 +0800

    Thu May 14th 2015

---
+++ FrankieAmap/FrankieAmap/Classes/FrankieFMDBHelper.h
@@ -0,0 +1,13 @@
+//
+//  FrankieFMDBHelper.h
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 5/14/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrankieFMDBHelper : NSObject
+
+@end
---
+++ FrankieAmap/FrankieAmap/Classes/FrankieFMDBHelper.m
@@ -0,0 +1,13 @@
+//
+//  FrankieFMDBHelper.m
+//  FrankieAmap
+//
+//  Created by Frankenstein Yang on 5/14/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "FrankieFMDBHelper.h"
+
+@implementation FrankieFMDBHelper
+
+@end
