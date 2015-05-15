736ee47ba33292f88ebbb20c68a850c5f7ead146
杨进忠 <fred.is.almighty@usa.com>
2015-05-15 14:31:00 +0800

    Fri May 15th 2015

---
+++ FrankieAmap/FrankieAmap WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  FrankieAmap WatchKit Extension
+//
+//  Created by Frankenstein Yang on 5/15/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
