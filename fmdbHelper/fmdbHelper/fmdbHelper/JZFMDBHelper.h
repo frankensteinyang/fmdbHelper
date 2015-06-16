90a847064eda2ae2a185a84e8d8a9b760d576504
杨进忠 <fred.is.almighty@usa.com>
2015-06-16 23:38:25 +0800

    Tue Jun 16th 2015

---
+++ fmdbHelper/fmdbHelper/fmdbHelper/JZFMDBHelper.h
@@ -0,0 +1,13 @@
+//
+//  JZFMDBHelper.h
+//  fmdbHelper
+//
+//  Created by Frankenstein Yang on 6/16/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JZFMDBHelper : NSObject
+
+@end
---
+++ fmdbHelper/fmdbHelper/fmdbHelper/JZFMDBHelper.m
@@ -0,0 +1,13 @@
+//
+//  JZFMDBHelper.m
+//  fmdbHelper
+//
+//  Created by Frankenstein Yang on 6/16/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "JZFMDBHelper.h"
+
+@implementation JZFMDBHelper
+
+@end
