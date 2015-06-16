fc14520c86d11403053ea61fdc09111cb9d194fe
杨进忠 <fred.is.almighty@usa.com>
2015-06-16 23:18:11 +0800

    Tue Jun 16th 2015

---
+++ fmdbHelper/fmdbHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fmdbHelper
+//
+//  Created by Frankenstein Yang on 6/16/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
