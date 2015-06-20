29043baffb8e0fc869a5e0bde73336a639412bfd
杨进忠 <fred.is.almighty@usa.com>
2015-06-20 23:37:45 +0800

    Sat Jun 20th 2015

--- fmdbHelper/fmdbHelper/fmdbHelper/JZFMDBHelper.m
+++ fmdbHelper/fmdbHelper/fmdbHelper/JZFMDBHelper.m
@@ -10,13 +10,21 @@
 
 @implementation JZFMDBHelper
 
-static FMDatabaseQueue *_queue;
+//static FMDatabaseQueue *_queue;
 
 + (void)initialize {
     // 获取沙盒中的数据库文件名
     NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"vipOne.sqlite"];
     // 在多个线程执行查询和更新，避免同时访问同一数据
-    _queue = [FMDatabaseQueue databaseQueueWithPath:path];
+//    _queue = [FMDatabaseQueue databaseQueueWithPath:path];
+}
+
++ (void)dropTable:(NSString *)tableName {
+
+}
+
++ (void)createTable:(NSString *)tableName withSqlParamDict:(NSDictionary *)dict {
+
 }
 
 @end
